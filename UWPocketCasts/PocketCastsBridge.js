d00a0db28415047044b1597ef3e3ef4c59047859
Conrad Kreyling <konistehrad@gmail.com>
2018-03-08 18:36:10 -0500

    Load JS from resource, not C# string

---
+++ UWPocketCasts/PocketCastsBridge.js
@@ -0,0 +1,88 @@
+﻿(function () {
+    const imageSelector = ".player-image .podcast-image .image-loaded";
+    var foundAudio;
+
+    function onEventPlaying(e) { window.external.notify('playing'); }
+    function onEventPause(e) { window.external.notify('pause'); }
+    function onEventAbort(e) { window.external.notify('abort'); }
+    function onEventEnded(e) { window.external.notify('ended'); }
+    function onEventSeeked(e) { window.external.notify('seeked'); }
+
+    function unregisterCallbacks(e) {
+        if (!e) return;
+        e.removeEventListener('playing', onEventPlaying);
+        e.removeEventListener('pause', onEventPause);
+        e.removeEventListener('abort', onEventAbort);
+        e.removeEventListener('ended', onEventEnded);
+        e.removeEventListener('seeked', onEventSeeked);
+    }
+
+    function registerCallbacks(e) {
+        if (!e) return;
+        e.addEventListener('playing', onEventPlaying);
+        e.addEventListener('pause', onEventPause);
+        e.addEventListener('abort', onEventAbort);
+        e.addEventListener('ended', onEventEnded);
+        e.addEventListener('seeked', onEventSeeked);
+    }
+
+    function jsWatch() {
+        try {
+            var currentFoundAudioEl = document.querySelector('audio');
+            if (foundAudio !== currentFoundAudioEl) {
+                if (foundAudio) {
+                    unregisterCallbacks(foundAudio);
+                }
+
+                foundAudio = currentFoundAudioEl;
+
+                if (foundAudio) {
+                    window.external.notify('audioFound');
+                    registerCallbacks(foundAudio);
+                }
+                else {
+                    window.external.notify('audioLost');
+                }
+            }
+        }
+        catch (e) {
+        }
+
+        setTimeout(jsWatch, 500);
+    }
+
+    window.pocketCastBridge = {
+        get isPlaying() {
+            return foundAudio && foundAudio.duration > 0 && !foundAudio.paused;
+        },
+
+        get durationInSeconds() {
+            return foundAudio ? foundAudio.duration : 0;
+        },
+
+        get positionInSeconds() {
+            return foundAudio ? foundAudio.currentTime : 0;
+        }, 
+
+        get jsonPlayerState() {
+            return JSON.stringify({
+                isPlaying: this.isPlaying,
+                durationInSeconds: this.durationInSeconds,
+                positionInSeconds: this.positionInSeconds,
+            });
+        },
+
+        pause() {
+            foundAudio && foundAudio.pause();
+        },
+
+        play() {
+            foundAudio && foundAudio.play();
+        },
+    };
+
+    setTimeout(jsWatch, 500);
+
+
+    return 'loaded';
+})();
--- UWPocketCasts/PocketCastsView.xaml.cs
+++ UWPocketCasts/PocketCastsView.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -19,25 +23,23 @@ namespace UWPocketCasts
 {
     public sealed partial class PocketCastsView : UserControl
     {
-        private const string InjectedJS = @"
-(function() {
-var foundAudio;
-
-function unregisterCallbacks()
-{
-}
-
-function jsWatch()
-{
-  
-}
-
-})();
-";
-
         public const string PocketCastsBaseURL = "https://playbeta.pocketcasts.com/web/";
         // <WebView x:Name="MainWebView" Source="https://playbeta.pocketcasts.com/web/"/>
         private WebView webView;
+        private bool kickedOffWatch;
+
+        public bool Playing { get; private set; }
+        public double DurationInSeconds { get; private set; }
+
+        private double positionInSeconds;
+        public double PositionInSeconds
+        {
+            get { return positionInSeconds; }
+            set
+            {
+                // dispatch async call to update scrub position
+            }
+        }
 
         public PocketCastsView()
         {
@@ -46,23 +48,99 @@ function jsWatch()
             webView = new WebView(WebViewExecutionMode.SeparateThread);
             WebViewBorder.Child = webView;
             webView.ScriptNotify += WebView_ScriptNotify;
+            webView.NavigationStarting += WebView_NavigationStarting;
+            webView.NavigationCompleted += WebView_NavigationCompleted;
             webView.Navigate(new Uri(PocketCastsBaseURL));
         }
 
-        private void WebViewBorder_Loaded(object sender, RoutedEventArgs e)
+        private void WebView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
         {
-
+            // reject navigation to non pocketcasts sites ...
+            Debug.WriteLine("WebView_NavigationStarting: " + args.Uri.Host);
+            if (!args.Uri.Host.Contains("pocketcasts.com", StringComparison.InvariantCultureIgnoreCase))
+            {
+                args.Cancel = true;
+            }
         }
 
-        private async void WatchForPlay()
+        private async Task<bool> InjectBridgeJS()
         {
+            try
+            {
+                StreamReader bridgeStream = new StreamReader(
+                    Assembly.GetExecutingAssembly().GetManifestResourceStream("UWPocketCasts.PocketCastsBridge.js")
+                );
 
+                string contents = await bridgeStream.ReadToEndAsync();
+                string[] evalArgs = new string[1];
+
+                evalArgs[0] = contents;
+                bool.Parse(await webView.InvokeScriptAsync("eval", evalArgs));
+
+                await SyncPlayerState();
+
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
         }
 
+        private async Task SyncPlayerState()
+        {
+            string[] evalArgs = new string[1];
 
+            evalArgs[0] = "window.pocketCastBridge.jsonPlayerState";
+            string jsonResult = await webView.InvokeScriptAsync("eval", evalArgs);
+            JsonObject obj = JsonValue.Parse(jsonResult).GetObject();
+            bool playing = obj.GetNamedBoolean("isPlaying");
+            double duration = obj.GetNamedNumber("durationInSeconds");
+            double position = obj.GetNamedNumber("positionInSeconds");
+
+            this.Playing = playing;
+            this.DurationInSeconds = duration;
+            this.positionInSeconds = position; // use field, not prop here!!!
+        }
+
+        private void WebView_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            Debug.WriteLine(string.Format("Nav complete {1}: {0}", args.Uri, args.IsSuccess));
+            if(!kickedOffWatch)
+            {
+                kickedOffWatch = true;
+                InjectBridgeJS();
+            }
+        }
+
+        private void WebViewBorder_Loaded(object sender, RoutedEventArgs e) { }
+        
         private void WebView_ScriptNotify(object sender, NotifyEventArgs e)
         {
-            // Respond to the script notification.
+            Debug.WriteLine(string.Format("ScriptNotify: {0}", e.Value));
+
+            string value = e.Value;
+
+            if (value == "playing")
+            {
+                Playing = true;
+            }
+            else if(value == "pause")
+            {
+                Playing = false;
+            }
+            else if(value == "ended")
+            {
+                Playing = false;
+            }
+            else if(value == "audioFound")
+            {
+
+            }
+            else if(value == "audioLost")
+            {
+                Playing = false;
+            }
         }
     }
 }
